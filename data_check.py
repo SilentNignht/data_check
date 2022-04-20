34dd9980ae4898914a45374d8da52dd1bc040822
KU,YUNG-HUNG <98871408+SilentNignht@users.noreply.github.com>
2022-04-20 11:39:56 +0800

    Update data_check.py
    update

--- data_check.py
+++ data_check.py
@@ -2,18 +2,20 @@ import pandas as pd
 
 
 def data_check(data):
-    df = pd.DataFrame(data, index=[0])
-    print(df.isnull().sum(axis=0))
-    # 檢查資料是否有遺漏值
-
+    #  檢查資料是否短缺
     if len(data) == 7:
-        #  檢查資料是否短缺
         for i in data.values():
             # 檢查資料格式是否正確
             if type(i) == type('a'):
                 continue
             else:
-                print(f"{i}:資料有問題")
-                break
+                return "資料格式錯誤"
+
     else:
-        print("資料短缺")
+        return "資料短缺"
+
+
+def data_check_null(data):
+    # 檢查資料是否遺漏
+    df = pd.DataFrame(test, index=[0])
+    print(df.isnull().sum(axis=0))
